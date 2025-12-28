b9d50f0771aa5de65ce5d47d60bb3a4459ea9f5b
Eender753030 <ender303030@gmail.com>
2025-12-28 22:06:48 +0800

    feat: Write main assembly logic

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,36 @@
+
+use risc_v_assembler::assembler::{self, parser};
+use risc_v_assembler::utils::{file, exception::AsmRiscVError};
+
 fn main() {
-    println!("Hello World!");
+    let mut instructions = Vec::new();
+
+    match file::read_asm("test.asm") {
+        Ok(content) => {
+            for line in content.lines() {
+                match parser::parse_instruction(line) {
+                    Ok(ins) => {
+                        println!("{:?}", ins);
+                        instructions.push(ins);
+                    }
+                    Err(e) => {
+                        match e {
+                            AsmRiscVError::ParseEmptyLine => continue,
+                            _ => eprintln!("{:?}", e)
+                        }
+                    }
+                }
+            }
+        },
+
+        Err(e) => {
+            eprintln!("{:?}", e);
+        }
+    } 
+
+    let binary_contents = assembler::assembly(&instructions);
+    println!("{:x?}", binary_contents);
+    if let Err(e) = file::write_binary("test", &binary_contents) {
+        eprintln!("{:?}", e);
+    }
 }
