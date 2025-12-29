074312f07c0bca3c7461c4c341f3a0906166059e
Eender753030 <ender303030@gmail.com>
2025-12-29 13:29:59 +0800

    feat: Implement S-type define and assembly

--- src/assembler/instruction.rs
+++ src/assembler/instruction.rs
@@ -2,4 +2,5 @@
 pub enum Instruction {
     Itype {rd: u32, rs1: u32, imm: i32, opcode: u32, funct3: u32},
     Rtype {rd: u32, rs1: u32, rs2: u32, opcode: u32, funct3: u32, funct7: u32},
+    Stype {rs1: u32, rs2: u32, imm: i32, opcode: u32, funct3: u32},
 }
